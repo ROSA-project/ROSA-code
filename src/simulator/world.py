05136b7f8ee1825825a697c0d195a8006ca8ee49
erfanz <erfan.zamanian@gmail.com>
2022-02-05 08:24:37 -0800

    adding comments to world

--- src/simulator/world.py
+++ src/simulator/world.py
@@ -1,3 +1,5 @@
+import time
+
 from intersection_instance import IntersectionInstance
 from map import Map
 from object import Object
@@ -5,18 +7,29 @@ from object import Object
 class World:
     """Maintains the state of the world including all its objects. There will be
        only one instance of this class, responsible for evolving the objects and
-       runs the main cycle. 
+       runs the main cycle.
+    
+    Attributes:
+        objects: a list of Objects
+        delta_t: world evolves every delta_t seconds.
+        __creation_ts: timestamp of world creation
+        __num_evolutions: how many evolutions have the world had so far
+        __duration_sec: determines how many seconds the world instance will exist for
+    
     """
+    
     def __init__(self, world_map: Map):        
-        self.__creation_ts = 0
+        self.__creation_ts = time.time() # current timestmap
         self.__num_evolutions = 0
-        
         self.objects: list[Object] = []
 
         #TODO hardcoded parameters here, to be taken care of properly
-        self.total_duration = 10 #in seconds
+        self.__duration_sec = 10
+            
                 
     def evolve(self, delta_t: float) -> None:
+        """Transitions the objects into next state.        
+        """
         for i in range(len(self.objects)):
             if not self.objects[i].owner_object:
                 #TODO direct access to object members
@@ -33,7 +46,7 @@ class World:
 
     def intersect(self) -> set[IntersectionInstance]:
         intersection_result = []
-        #TODO decide on strcucture
+        #TODO decide on structure
         
         for i in range(len(self.objects)):
             for j in range(len(self.objects)):
@@ -42,26 +55,23 @@ class World:
         return intersection_result
     
     def run(self) -> None:
-        delta_t_list=[self.total_duration+1]
-        #meaning that we need only one round 
-        #and the run loop will work only for the initial t.
+        #TODO: For now, we will only  run the world for one round
+        delta_t_list=[self.__duration_sec+1]
         
         for object in self.objects:
             delta_t_list.append(object.get_required_delta_t())
         self.delta_t = min(set(delta_t_list)-{0})
 
         #TODO the delta_t based on movement of objects, decided by the world, goes here
-            
+        
         t = 0
-        #TODO t to be initialized if an initial state is given
-
-        while t < self.total_duration:
+        while t < self.__duration_sec:
             self.intersection_result = self.intersect()
-            self.evolve(self.delta_t)
+            self.evolve(delta_t)
             t = t + self.delta_t
+            self.__num_evolutions += 1
 
     def add_object(self, new_objects: list[Object]) -> None:  
-        #TODO can we specify a list with these type hints?
-
+        """Appends new objects to the list of world objects.
+        """
         self.objects.extend(new_objects)
-        #TODO what if new_objects is not a list, but a single object?
