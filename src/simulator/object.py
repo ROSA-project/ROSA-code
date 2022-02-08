dc736e405a4aa646d1345d3c743e766e34cff2e6
erfanz <erfan.zamanian@gmail.com>
2022-02-07 23:30:45 -0800

    object.evolve now doesn't expect intersections

--- src/simulator/object.py
+++ src/simulator/object.py
@@ -3,37 +3,56 @@ from __future__ import annotations
 from shape import Shape
 from position import Position
 from intersection_instance import IntersectionInstance
+from object import Object
 
 ObjectId = int # type aliasing, since we might change the composition of ObjectId in future
 
 class Object:
-    def __init__(self, oid: ObjectId, shape: Shape, position: Position, owner_object):
-        #TODO can't type hint owner_object because it doesn't know Object yet :D
-        self.id = oid
+    def __init__(self, oid: ObjectId, shape: Shape, position: Position, owner_object: Object):
+        self.oid = oid
         self.shape = shape
-        #shape could be None
         self.position = position
         self.owner_object = owner_object
-        self.dependent_objects = []
-    
-    def evolve(self, delta_t: int, intersection_results: list[IntersectionInstance]) -> dict[ObjectId, Object]:
-        #TODO saeed: could this be the default evolve method?
-        collected_offspring_objects = []
+        self.dependent_objects: dict[ObjectId, Object] = {}
 
-        for object in self.dependent_objects:
-            offspring_objects = object.evolve(delta_t, intersection_result)
-            collected_offspring_objects.extend(offspring_objects)
-        
-        self.dependent_objects.extend(collected_offspring_objects)
-        return collected_offspring_objects
+    def add_dependent_object(self, obj: Object):
+        self.dependent_objects[obj.oid] = obj
+    
+    def evolve(self, delta_t: float) -> dict[ObjectId, Object]:
+        """Transitions the object and all its dependents to the next state.
+
+        Args:
+            delta_t: span of evolution in seconds
+
+        Returns:
+            A dictionary of all the new objects which are created by this object.
+            These new objects will be added to the list of dependent objects.
+        """
+
+        #TODO: evolve should access to the list of intersections in this iteration.
+        #TODO: evolve should actually change the state. We currently do not change
+        # Position or Speed
+        offspring_objects: dict[ObjectId, Object] = {}
+        for oid in self.dependent_objects:
+            offsprings = self.dependent_objects[oid].evolve(delta_t)
+            offspring_objects.update(offsprings)
+
+        # add the new offsprings to the list of dependents
+        self.dependent_objects.update(offspring_objects)
+        return offspring_objects
 
     def set_intersections(self, intersections: list[IntersectionInstance]) -> None:
+        """Registers this round's intersections to be later used by evolve().
+        """
+        #TODO: should it be simply stored in a member variable?
         pass
     
     def visualize(self):
+        #TODO: define return type
         pass
     
     def bounding_box(self):
+        #TODO: define return type
         pass
 
     def get_required_delta_t(self) -> float:
@@ -43,7 +62,11 @@ class Object:
         return 0
         
     def time_to_die(self) -> bool:
+        """Checks if object must cease to exist in the next iteration of the world.
+        """
         return False
 
     def is_evolvable(self) -> bool:
+        """Checks if object should be evolved directly by the world.
+        """
         return not self.owner_object
