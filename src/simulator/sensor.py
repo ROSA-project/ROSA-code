7761e979b813bdaca356414e2c65bd235d905f18
Mohammad <md.kameli@gmail.com>
2022-02-19 17:43:33 +0100

    Sensor module created.

--- src/simulator/sensor.py
+++ src/simulator/sensor.py
@@ -1,5 +1,14 @@
-from object import Object
+from object import Object, ObjectId
+from shape import Shape
+from position import Position
+# from intersection_instance import IntersectionInstance #for the next version
 
 
 class Sensor(Object):
-    pass
+    # def sense(self, intersections: list[IntersectionInstance]) -> bool #for the next version
+    def __init__(self, oid: ObjectId, shape: Shape, position: Position, owner_object: Object):
+        super().__init__(oid, shape, position, owner_object)
+
+    def sense(self):
+        pass
+
