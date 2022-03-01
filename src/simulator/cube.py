173fee20da0514c30c5cb5c835a5862f1c8d7694
salehfazaeli <s.fazaeli.h@gmailcom>
2022-03-01 03:36:46 +0330

    remove conflicts

--- src/simulator/cube.py
+++ src/simulator/cube.py
@@ -1,12 +1,3 @@
-<<<<<<< Updated upstream
-from simulator.shape import Shape
-
-
-class Cube (Shape) -> None
-        lenght.self = lenght
-        width.self = width
-        height.self = height
-=======
 class Cube(Shape):
     def __init__(self,lenght,height,width):
         self.lenght = lenght
@@ -15,4 +6,3 @@ class Cube(Shape):
     
     def bounding_box(shape):
         pass
->>>>>>> Stashed changes
