46a4959ad3156425fa366b7b046d073b60daff26
Mohammad <md.kameli@gmail.com>
2022-03-07 02:34:01 +0100

    **kwargs removed

--- src/simulator/vacuum_cleaner.py
+++ src/simulator/vacuum_cleaner.py
@@ -2,6 +2,6 @@ from robot import Robot
 
 
 class VacuumCleaner(Robot):
-    def evolve(self, **kwargs):
+    def evolve(self):
         pass
     # TODO the evolve method should be defined and completed.
