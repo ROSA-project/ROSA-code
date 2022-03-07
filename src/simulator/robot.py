312a25b72fb4944bb45f98370c6c2235124cc866
Mohammad <md.kameli@gmail.com>
2022-03-07 02:32:58 +0100

    An evolve method added but need to be discussed

--- src/simulator/robot.py
+++ src/simulator/robot.py
@@ -1,14 +1,34 @@
 from __future__ import annotations
 from object import Object
-from position import Position
-from object import ObjectId
-from shape import Shape
+from bumper_sensor import BumperSensor
 
 
 class Robot(Object):
-    def __init__(self, oid: ObjectId, shape: Shape, position: Position, owner_object: Object):
-        super().__init__(oid, shape, position, owner_object)
+    def __init__(self, x: int, y: int, z: int, phi: int, theta: int):
+        self.x = x
+        self.y = y
+        self.z = z
+        self.phi = phi
+        self.theta = theta
+        #  super().__init__(oid, shape, position, owner_object)
 
-    def evolve(self):
-        pass  # this method is going to be overridden in the children classes of VacuumCleaner
+    def evolve(self, bump: bool):
+        """Transitions the object and all its dependents to the next state.
+
+        Args:
+            A Boolean bump signal
+
+        Returns:
+            The robot ID and its Position
+        """
+        if bump:
+            print('bump')
+            for i in range(5):
+                self.phi += 10
+        else:
+            self.x += 1
+            self.y += 1
+        return [self.x, self.y, self.z, self.phi, self.theta]
+
+
 
