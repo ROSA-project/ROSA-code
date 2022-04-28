6a9142fd218e1d1e790d035119b9065a8acd483c
saeedafra <saeed.afrasiabi@gmail.com>
2022-04-28 19:11:03 +0200

    set up unittests for decomposing of a rectangle into lines

---
+++ src/tests/test_intersection_rectangle_circle.py
@@ -0,0 +1,31 @@
+import unittest
+import numpy as np
+import sys
+import json
+sys.path.append("simulator")
+
+import tests.testing_tools as teto
+import intersection_instance as in_in
+
+
+class PointObjectBounce(unittest.TestCase):
+    def test_rectangle_to_lines_decomposition(self):
+        json_filename = "tests/rectangle_to_lines_decomposition.json"
+        with open(json_filename, "r") as f:
+            testcases = json.load(f)
+        
+        test_counter = 0
+        for testcase in testcases:
+            with self.subTest(i = test_counter):
+                lines = in_in.IntersectionInstance.rectangle_lines(\
+                    testcases[testcase]["rectangle_center"], \
+                    testcases[testcase]["x_axis_side"], \
+                    testcases[testcase]["y_axis_side"], \
+                    testcases[testcase]["phi_degree"] * np.pi/180)
+        
+                self.assertTrue(teto.matrix_almost_equal(lines , 
+                            testcases[testcase]["expected_lines"]), msg = testcase)                
+            test_counter += 1
+
+if __name__ == '__main__':
+    unittest.main()
