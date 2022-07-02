da28b466741baee2ed1a08dd9f93cd5b2823c0ca
Mohammad mehdi ahmadi <98178526+mehdi138r@users.noreply.github.com>
2022-07-02 18:38:17 +0430

    #2-improve names and comments

--- src/simulator/beam.py
+++ src/simulator/beam.py
@@ -1,8 +1,8 @@
 import non_physical_object
 
 
-class Beam(non_physical_object.Non_physical_object):
+class Beam(non_physical_object.NonPhysicalObject):
     """
-    One of the robot objects
+     to be used as the light beam (or whatever) used by the distance measuring sensors
     """
     pass
