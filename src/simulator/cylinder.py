cc08fa1b91bd7b55967f1c90d2522ce22bc7f377
Mohammad mehdi ahmadi <98178526+mehdi138r@users.noreply.github.com>
2022-07-25 16:11:49 +0430

    #15-improve comments(bad explanation),improve code(combine list, if I'm not wrong)

--- src/simulator/cylinder.py
+++ src/simulator/cylinder.py
@@ -24,15 +24,16 @@ class Cylinder(Shape):
         # TODO: The following calculation is only for an upright cube
         r = self.radius
         h = self.height
+
         # Because the cylinder has symmetry, we only need the top side points of the disk.
         # We calculate the distance of the farthest points and then get the dimensions
         # of the bounding box. Note, since we used the top points, because of symmetry,
         # we double the dimensions of the bounding box so that the box surrounds the bottom
         # points of the disk.
         points = [[r, 0, h / 2], [0, r, h / 2], [-r, 0, h / 2], [0, -r, h / 2]]
-        new_x = list()
-        new_y = list()
-        new_z = list()
+
+        # like [x-points, y-points, z-points]
+        new_points = [[], [], []]
         for point in points:
             if position.theta % 180 == 0:
                 # Notice: At these angles, theta does not change the shape,
@@ -41,16 +42,24 @@ class Cylinder(Shape):
                 new_point = Shape.rotation(point[0], point[1], point[2], 0, 0)
             else:
                 new_point = Shape.rotation(point[0], point[1], point[2], position.phi, position.theta)
-            new_x.append(new_point[0])
-            new_y.append(new_point[1])
-            new_z.append(new_point[2])
-        length = 2 * max([abs(x) for x in new_x])
-        width = 2 * max([abs(y) for y in new_y])
-        height = 2 * max([abs(z) for z in new_z])
-        bounding_box = Box(oid=None, name=None, cube=Cube(length=length, width=width, height=height),
-                           position=Position(position.x, position.y, position.z, 0, 0),
-                           owner_object=None,
-                           registry=None)
+            new_points[0].append(new_point[0])
+            new_points[1].append(new_point[1])
+            new_points[2].append(new_point[2])
+
+        # Due to the symmetry of the shape, only the top points of
+        # the disk are considered. In order to consider the bottom points
+        # of the disk in the bounding box, the value is doubled.
+        # For the surrounding box to be surrounded by the shape,
+        # the farthest distance to the point of each axis is needed
+        # (the reason for using max), the points may be in the negative area,
+        # and due to the need for the farthest distance, the size is important (the reason for using abs).
+        length = 2 * max([abs(x) for x in new_points[0]])
+        width = 2 * max([abs(y) for y in new_points[1]])
+        height = 2 * max([abs(z) for z in new_points[2]])
+
+        bounding_box = Box(None, None, Cube(length=length, width=width, height=height),
+                           Position(position.x, position.y, position.z, 0, 0),
+                           None, None)
         return bounding_box
 
     def dump_info(self) -> dict:
