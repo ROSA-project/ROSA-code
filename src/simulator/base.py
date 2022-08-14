e15aa071cdb1d91cac3ea7df4fa74e1273b42383
Mohammad mehdi ahmadi <98178526+mehdi138r@users.noreply.github.com>
2022-08-14 16:53:04 +0430

    #2-update class base constructor, add two methods

--- src/simulator/base.py
+++ src/simulator/base.py
@@ -1,9 +1,27 @@
-from object import Object
+from object import Object, ObjectId
+from position import Position
+from shape import Shape
+from object_registry import ObjectRegistry
+from robot import Robot
+import numpy as np
 
 RadioID = str
 
 
 class Base(Object):
-    def __int__(self, oid, name, shape, position, owner_object, registry, rid: RadioID):
+    def __init__(self, oid: ObjectId, name: str, shape: Shape, position: Position,
+                 owner_object: Object, registry: ObjectRegistry, rid: RadioID, error: int):
         super().__init__(oid, name, shape, position, owner_object, registry)
         self.radio_id = rid
+        self.error = error
+
+    def get_distance(self, robot: Robot):
+        delta_x = self.position.x - robot.position.x
+        delta_y = self.position.y - robot.position.y
+        delta_z = self.position.z - robot.position.z
+        true_distance = np.sqrt(delta_x ** 2 + delta_y ** 2 + delta_z ** 2)
+        distance = true_distance + np.random.normal(loc=0, scale=self.error)
+        return distance
+
+    def evolve(self):
+        pass
