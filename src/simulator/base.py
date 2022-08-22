d48745bfe1246fc84936ccbad9b081017a0fc811
Mohammad mehdi ahmadi <98178526+mehdi138r@users.noreply.github.com>
2022-08-23 04:06:01 +0430

    update methods, add one variable in constructor.

--- src/simulator/base.py
+++ src/simulator/base.py
@@ -1,6 +1,6 @@
 import object
-from position import Position
-from shape import Shape
+import position
+import shape
 import object_registry
 import numpy as np
 
@@ -13,37 +13,35 @@ class Base(object.Object):
     and facilitating their navigation by transmitting their (estimated) distances with the base.
     """
 
-    def __init__(self, oid: object.ObjectId, name: str, shape: Shape, position: Position,
+    def __init__(self, oid: object.ObjectId, name: str, shape: shape.Shape, position: position.Position,
                  owner_object: object.Object, registry: object_registry.ObjectRegistry,
-                 rid: RadioID, standard_deviation: float):
+                 rid: RadioID, standard_deviation: float, time_using : float):
         super().__init__(oid, name, shape, position, owner_object, registry)
         self.radio_id = rid
         self.standard_deviation = standard_deviation
         self.__timer = 0
+        self.__time_using = time_using
 
-    def get_distance(self, position: Position):
+    def get_distance(self, position: position.Position) -> float:
         """
         It calculates and returns the distance between itself and the robot with some error.
         """
-        # TODO: The value of 0.05 is temporary, it should be modified later.
-        if self.__timer % 0.05 == 0:
+        # TODO: Temporarily, this method takes a position. It should change later.
+
+        if self.__timer == self.__time_using:
             delta_x = self.position.x - position.x
             delta_y = self.position.y - position.y
             delta_z = self.position.z - position.z
             true_distance = np.sqrt(delta_x ** 2 + delta_y ** 2 + delta_z ** 2)
             distance = true_distance + np.random.normal(loc=0, scale=self.standard_deviation)
+            self.__timer = 0
             return distance
         else:
             return None
 
-    def evolve(self, delta_t: float) -> dict[object.ObjectId, object.Object]:
+    def evolve(self, delta_t: float):
         """
         Just like in the object class, only time is added to the timer
         """
         self.__timer += delta_t
-        offspring_objects: dict[object.ObjectId, object.Object] = {}
-        for oid in self.dependent_objects:
-            offsprings = self.dependent_objects[oid].evolve(delta_t)
-            offspring_objects.update(offsprings)
-
-        return offspring_objects
+        object.Object.evolve(self, delta_t)
