bc4024fcf95d9c895d244a6ca1aa6c1d337e3bc8
Mohammad mehdi ahmadi <98178526+mehdi138r@users.noreply.github.com>
2022-08-31 11:08:41 +0430

    update sense method, add some comments and edit the type hint in position class.

--- src/simulator/distance_measuring_sensor.py
+++ src/simulator/distance_measuring_sensor.py
@@ -4,16 +4,35 @@ import position
 import cylinder
 import object_registry
 import beam
+import copy
+import numpy as np
 
 
 class DistanceMeasuringSensor(sensor.Sensor):
+    """
+    it is a sensor that measures the distance of an object to where the sensor is located.
+    """
     def __int__(self, oid: object.ObjectId, name: str, pos: position.Position,
                 owner_object: object.Object, registry: object_registry.ObjectRegistry, parameter: float):
+        self.position = pos
+        new_position = copy.copy(self.position)
+        new_position.x = 0.5 * parameter * np.cos(self.position.phi)
+        new_position.y = 0.5 * parameter * np.sin(self.position.phi)
+        new_position.theta = 90
+
         self.beam_object = beam.Beam(registry.get_next_available_id(), name, cylinder.Cylinder(0, parameter),
-                                     pos, owner_object, registry)
+                                     new_position, owner_object, registry)
 
-    def sense(self):
+    def sense(self) -> float:
+        """
+        it measures the distance between the robot and any object.
+        if it isn't in the range, it returns an unreasonable number.
+        """
         for in_in in self.beam_object._latest_intersections:
-            if in_in.does_intersect() and in_in.is_infinitesimal():
-                return self.beam_object.shape.height
-
+            if in_in.does_intersect():
+                delta_x = in_in.get_intersection_point()[0][0] - self.position.x
+                delta_y = in_in.get_intersection_point()[0][1] - self.position.y
+                distance = np.sqrt(delta_x**2 + delta_y**2)
+                return distance
+            else:
+                return -1
